d209aade8548dbd017f0a5772966a2faaec12591
Vu Tran <vutran6853@gmail.com>
2024-11-03 10:46:10 -0600

    add dev package

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      About page
+      <a href="/" rel="noopener noreferrer">
+        Home
+      </a>
+    </div>
+  )
+}
